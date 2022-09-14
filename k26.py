c8e1dacd9a6e1663a6cce75580f3529563d49f44
Josahn Alfonso-Schneider <112727167+josahn@users.noreply.github.com>
2022-09-14 19:37:16 +0000

    updaute

---
+++ k26.py
@@ -0,0 +1,43 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
--- lang52a.py
+++ lang52a.py
@@ -1,5 +1,5 @@
 length = int(input("please enter length"))
-width = int(input("please input width"))
+width = int(input("please enter width"))
 
 area = length + width
 perin = 2 * length + 2 * width
