dda7782480f9d1a327c8e088b3a1b27a3b19ca08
Josahn Alfonso-Schneider <112727167+josahn@users.noreply.github.com>
2022-09-16 19:33:57 +0000

    a couple karel chapter 2 assignments

--- k35.py
+++ k35.py
@@ -2,41 +2,41 @@ from stanfordkarel import *
 
 
 class ktools:
-    def m(self):
-    """shorthand for move"""
+   def m(self):
+    """move"""
     move()
 
-    def tl(self):
+   def tl(self):
     """turn left"""
     turn_left()
 
-    def tr(self):
+   def tr(self):
     """turn right"""
     self.tl()
     self.tl()
     self.tl()
 
-    def ta(self):
+   def ta(self):
     """turn around"""
     self.tl()
     self.tl()
 
-    def pick(self):
+   def pick(self):
     """pick beeper"""
     pick_beeper()
 
 
-    def put(self):
+   def put(self):
     """put beeper"""
     put_beeper()
 
-    def put2(self):
+   def put2(self):
     """put 2 beepers in a line"""
     self.put()
     self.m()
     self.put()
 
-    def put5(self):
+   def put5(self):
     """put 5 beepers in a line"""
     self.put2()
     self.m()
@@ -44,7 +44,7 @@ class ktools:
     self.m()
     self.put()
 
-    def h(self):
+   def h(self):
     """print it using beepers"""
     self.tl()
     self.put5()
@@ -62,18 +62,18 @@ class ktools:
     self.tl()
     self.m4()
 
-    def m3(self):
+   def m3(self):
     """move three times"""
     self.m()
     self.m()
     self.m()
 
-    def m2(self):
+   def m2(self):
     """move two times"""
     self.m()
     self.m()
 
-    def m4(self):
+   def m4(self):
     """move four times"""
     self.m()
     self.m()
@@ -82,11 +82,27 @@ class ktools:
     
 
      
-    def main():
+def main():
     """ Karel code goes here! """
     kt = ktools()
     kt.h()
-  
+    kt.put2()
+    kt.m()
+    kt.put()
+    kt.tl()
+    kt.tl()
+    kt.m2()
+    kt.tr()
+    kt.put5()
+    kt.tr()
+    kt.m()
+    kt.put2()
+    kt.tr()
+    kt.m2()
+    kt.put()
+    kt.tr()
+    kt.m()
+    kt.put()
     pass
 
 
