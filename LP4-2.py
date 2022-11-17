d983bad14bb3eefe7673262c80b5a8777b7356e0
Josahn Alfonso-Schneider <112727167+josahn@users.noreply.github.com>
2022-11-17 20:34:56 +0000

    UPDATE

--- LP4-2.py
+++ LP4-2.py
@@ -1,7 +1,19 @@
 def main():
+  W = int(input("Enter weight in kilograms:"))
+  L = int(input("Enter length in cubic centimeters:"))
+  H = int(input("Enter height in cubic centimeters:"))
+  w = int(input("Enter width in cubic centimeters:"))
 
+  if W > 0 and  W <= 27: print("")
+  else: print("too heavy")
+
+  if L * H * w > 0 and L * H * w <= 100000: print("")
+  else: print("too large")
+
+  if W > 27 and L * H * w > 100000: print("too heavy and large")
+  
   pass
 
 
-if__name__=="__main__":
+if __name__=="__main__":
   main()
